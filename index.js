cefdfc099e917b1415c4f19912a67d8f7d5c3d97
TengenTopppaGurrenLaggan <59069576+TengenTopppaGurrenLaggan@users.noreply.github.com>
2019-12-27 08:36:17 +0200

    add

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const http = require('http')
 //const hostname '127.0.0.1'
 const hostname= herokuapp.com
-const port = thawing-citadel-21048
+const port = thawing-citadel-21048.
 const server = http.createServer((req, res) => {
     res.statusCode = 200
     res.setHeader('Content-Type', 'text/plain')
